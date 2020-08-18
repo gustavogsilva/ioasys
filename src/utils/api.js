9c92a2d4fa59a08ba037cb668e05872560a51a17
Gustavo <chdrlz@gmail.com>
2020-08-18 18:24:55 -0300

    Comments removed

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,12 +5,3 @@ export const API_URL = "https://empresas.ioasys.com.br/api/v1";
 export const getRequest = path => {
   return axios.get(API_URL + path).then(res => res.data);
 };
-
-// try {
-//   const res = await axios.get(API_URL + path);
-//   return res.data;
-// } catch (error) {
-//   console.error(error);
-//   if (error.response === undefined) alert("O servidor está fora do ar, tente novamente mais tarde.");
-//   return false;
-// }
